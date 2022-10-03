889d7fa9cee2980753bfd0a4e39c9b7d39cab8b6
LauratHaaga <103933390+LauratHaaga@users.noreply.github.com>
2022-10-03 18:52:16 +0200

    Adding imports for preact

--- useTimer.js
+++ useTimer.js
@@ -1,36 +1,40 @@
-function useTimer({ endDate, startDate, delay }) {
-  const targetDate = React.useRef(new Date(endDate).getTime());
-  const [countDown, setCountDown] = React.useState({ finished: false });
-  const timer = React.useRef();
+import { h } from 'preact';
+import { useState, useEffect, useRef } from 'preact/hooks';
 
-  React.useEffect(() => {
-    if (new Date(startDate).getTime() <= new Date().getTime()) {
-      timer.current = setInterval(() => {
-        const now = new Date().getTime();
-        const diff = targetDate.current - now;
-
-        if (diff <= 0) {
-          setCountDown((prev) => ({ ...prev, finished: true }));
-          clearInterval(timer.current);
-          return;
+export default function useTimer({ endDate, startDate, delay }) {
+    const targetDate = useRef(new Date(endDate).getTime());
+    const [countDown, setCountDown] = useState({ finished: false });
+    const timer = useRef();
+  
+    useEffect(() => {
+        if (new Date(startDate).getTime() <= new Date().getTime()) {
+            timer.current = setInterval(() => {
+                const now = new Date().getTime();
+                const diff = targetDate.current - now;
+  
+                if (diff <= 0) {
+                    setCountDown((prev) => ({ ...prev, finished: true }));
+                    clearInterval(timer.current);
+                    return;
+                }
+  
+                const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+                const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+                const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  
+                setCountDown({
+                    finished: false,
+                    days,
+                    hours,
+                    minutes,
+                    seconds,
+                });
+            }, delay);
         }
-
-        const days = ~~(diff / (1000 * 60 * 60 * 24));
-        const hours = ~~((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = ~~((diff % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = ~~((diff % (1000 * 60)) / 1000);
-
-        setCountDown({
-          finished: false,
-          days,
-          hours,
-          minutes,
-          seconds
-        });
-      }, delay);
-      return () => clearInterval(timer.current);
-    }
-  }, []);
-
-  return countDown;
+        
+        return () => clearInterval(timer.current);
+    }, []);
+  
+    return countDown;
 }
